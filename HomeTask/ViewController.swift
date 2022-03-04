8c350c1715dbddde2fe85eb95ec3073af102718f
WebVajhegan <webvajhegan@webvajhegans-macbook-pro.local>
2022-03-04 20:20:29 +0330

    Initial Commit

---
+++ HomeTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeTask
+//
+//  Created by WebVajhegan on 3/4/22.
+//  Copyright © 2022 WebVajhegan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
